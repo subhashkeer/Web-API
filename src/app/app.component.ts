eac6bc1a99a002a6a5b63daceedaff2ec3d7297e
subhashkeer <subhashkeer@gmail.com>
2018-09-20 01:33:47 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import{ Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-api';
+  private apiUrl = "https://address-book-demo.herokuapp.com/api/contacts";
+  data:any = {};
+   
+  constructor(private http: Http){
+    console.log('Hello Fellow User');
+    this.getContacts();
+    this.getData();
+  }
+  getData(){
+    return this.http.get(this.apiUrl)
+    .map((res: Response) => res.json())
+  }
+
+  getContacts(){
+    this.getData().subscribe(data=>{
+      console.log(data);
+      this.data = data;
+    })
+  }
 }
